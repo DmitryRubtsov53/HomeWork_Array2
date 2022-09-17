9540dd7d278bb0b096b12abb440191c853fb066f
DmitryRubtsov53 <2578390@mail.ru>
2022-09-17 09:19:17 +0500

    Внесены исправления, указанные наставником. Задачи № 2 и № 4 ДЗ Урока "Массивы 2" решенф двумя способами (2-й указан наставником).

--- src/Main.java
+++ src/Main.java
@@ -74,19 +74,3 @@ public class Main {
             }
     }
 }
-
-/*      К СОЖАЛЕНИЮ, С ВЫЗОВОМ МЕТОДА У МЕНЯ НЕ ВЫШЛО !!!
-        Постоянно IDEA выдавала ошибку и я не поняд почему !!!!!
-
-        int[] arr = generateRandomArray();
-
-        public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
-            System.out.println(arr[i]);
-        }
-        return arr;
-    }
-*/
