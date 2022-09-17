29618a474d9ce6790b872ce4932b1c9277d88f07
DmitryRubtsov53 <2578390@mail.ru>
2022-09-17 09:14:55 +0500

    Решены все 4 задачи ДЗ Урока Массивы 2

--- src/Main.java
+++ src/Main.java
@@ -21,15 +21,24 @@ public class Main {
         System.out.println("Сумма трат за месяц составила " + summa + " рублей");
 
         System.out.println();
-        System.out.println("Задача 2 - Найти минимальную и максимальную трату за день.");
+        System.out.println("Задача 2 - Найти минимальную и максимальную трату в день.");
 
         Arrays.sort(arr);
         // Для себя - Распечатаем отсортированный по возрастанию массив для сравнения с MAX и MIN:
         System.out.println(Arrays.toString(arr));
-
+        /* Вариант 1. С помощью сортировки.
         int maxTrata = arr[arr.length -1], minTrata = arr[0];
-        System.out.println("Минимальная сумма трат за день составила " + maxTrata + " рублей");
+        System.out.println("Максимальная сумма трат за день составила " + maxTrata + " рублей");
         System.out.println("Минимальная сумма трат за день составила " + minTrata + " рублей");
+        */
+        // Вариант 2. Классическим перебором элементов с сравнением.
+        int maxEl = 0, minEl = 200_000;  // Переменные для хранения значений макс-ого и мин-ого значения элемента массива.
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] > maxEl)  maxEl = arr[j];
+            if (arr[j] < minEl)  minEl = arr[j];
+        }
+        System.out.println("Максимальная сумма трат в день составила " + maxEl + " рублей");
+        System.out.println("Минимальная сумма трат в день составила " + minEl + " рублей");
 
         System.out.println();
         System.out.println("Задача 3 - Найти средную сумму трат за месяц.");
@@ -38,29 +47,35 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             sumMonth += arr[i];
         }
-        double sredaySum = sumMonth / arr.length;
-        System.out.println("Средняя сумма трат за месяц составила " + Math.ceil(sredaySum) + " рублей");
+        double averageSum = sumMonth / arr.length;
+        System.out.println("Средняя сумма трат за месяц составила " + Math.ceil(averageSum) + " рублей");
 
         System.out.println();
         System.out.println("Задача 4 - Исправить баг с выводом ФИО сотрудника.");
 
         char[] reverseFullName = {'n','a','v','I',' ','v','o','n','a','v','I'};
 
-        System.out.println("Исправлено:");
 
-        char[] FullName = new char[reverseFullName.length];
+        System.out.println("Исправлено:");
+        /* Вариант 1. С созданием "правильного" массива и его распечаткой.
+        char[] fullName = new char[reverseFullName.length];
         int j = reverseFullName.length;
 
-        for (int i = 0; i < FullName.length; i++) {
+        for (int i = 0; i < fullName.length; i++) {
 
-            FullName[i] = reverseFullName[j-1];
+            fullName[i] = reverseFullName[j-1];
             j--;
-            System.out.print(FullName[i]);
+            System.out.print(fullName[i]);
+            }
+        */
+        // Вариант 2. Без созданием "правильного" массива, который мог бы использоваться потом бухгалтерией.
+        for (int i = reverseFullName.length - 1; i >=0 ; i--) {
+            System.out.print(reverseFullName[i]);
             }
     }
 }
 
-/*      К СОЖАЛЕНИЮ, С ВЫЗОВОМ МЕТОДА ИЗ НИЧЕГО У МЕНЯ НЕ ВЫШЛО !!!
+/*      К СОЖАЛЕНИЮ, С ВЫЗОВОМ МЕТОДА У МЕНЯ НЕ ВЫШЛО !!!
         Постоянно IDEA выдавала ошибку и я не поняд почему !!!!!
 
         int[] arr = generateRandomArray();
